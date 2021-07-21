83fda9c1c62baa1a09df5b955c2f1354d8c108d9
Shubham Mehrotra <shubh0896m@gmail.com>
2021-07-21 16:08:14 +0530

    updates

--- src/Http/Controllers/Setting/AttributeController.php
+++ src/Http/Controllers/Setting/AttributeController.php
@@ -32,13 +32,16 @@ class ActivityController extends Controller
         $id = request('id');
 
         $activity = $this->activityRepository
-                    ->select('lead_activities.id', 'users.name as organizer_name')
+                    ->select('lead_activities.id', 'users.name as organizer_name', 'schedule_from', 'schedule_to', 'comment')
                     ->leftJoin('users', 'lead_activities.user_id', '=', 'users.id')
                     ->where('lead_activities.id', $id)
                     ->first();
 
         $icsContent =   view('calendarapp::ics.sample-ics', [
-                            'organizerName' => $activity->organizer_name
+                            'summary'       => $activity->comment,
+                            'organizerName' => $activity->organizer_name,
+                            'scheduleTo'    => (new \DateTime($activity->schedule_to, new \DateTimeZone("UTC")))->format('YdmThisZ'),
+                            'scheduleFrom'  => (new \DateTime($activity->schedule_from, new \DateTimeZone("UTC")))->format('YdmThisZ'),
                         ])->render();
 
         return response()->json([
